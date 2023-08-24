838245d4806c76f7eafa0019a6f8c90161770c30
Swapnil Singh <swapnilskumars@gmail.com>
2023-08-24 14:33:34 +0530

    Completed till rock paper scissors js

--- JS/CleverProgrammer/index.js
+++ JS/CleverProgrammer/index.js
@@ -15,3 +15,69 @@ const change_heading_func = () => {
         heading_div.innerHTML = "<h1>This is Heading 1</h1>"
     }
 }
+
+// Rock Paper Scissors
+const rps_comp_generate = () => {
+    let r_num = Math.floor(Math.random() * 3);
+    if(r_num == 0){
+        return "Rock"
+    }else if(r_num == 1){
+        return "Paper"
+    }else{
+        return "Scissor"
+    }
+}
+const win_check = (r_comp, r_human) => {
+    if(r_comp == r_human){
+        return "It's a Draw"
+    }else if(
+        (r_human == "Paper" && r_comp == "Rock")||
+        (r_human == "Scissor" && r_comp == "Paper")||
+        (r_human == "Rock" && r_comp == "Scissor")
+    ){
+        return "Human Wins"
+    }else if(
+        (r_comp == "Paper" && r_human == "Rock")||
+        (r_comp == "Scissor" && r_human == "Paper")||
+        (r_comp == "Rock" && r_human == "Scissor")
+    ){
+        return "Computer Wins"
+    }
+}
+
+const click_rock = () => {
+    r_comp = rps_comp_generate()
+    r_human = "Rock"
+    console.log("Computer : " + r_comp)
+    console.log("Human : " + r_human)
+    let winner = win_check(r_comp, r_human)
+    console.log(winner)
+    r_human_text = document.getElementById("r_human")
+    r_comp_text = document.getElementById("r_comp")
+    r_winner = document.getElementById("winner_text")
+    r_human_text.innerText = r_human
+    r_comp_text.innerText = r_comp
+    r_winner.innerText = winner    
+}
+const click_paper = () => {
+    r_comp = rps_comp_generate()
+    r_human = "Paper"
+    let winner = win_check(r_comp, r_human)
+    r_human_text = document.getElementById("r_human")
+    r_comp_text = document.getElementById("r_comp")
+    r_winner = document.getElementById("winner_text")
+    r_human_text.innerText = r_human
+    r_comp_text.innerText = r_comp
+    r_winner.innerText = winner  
+}
+const click_scissor = () => {
+    r_comp = rps_comp_generate()
+    r_human = "Scissor"
+    let winner = win_check(r_comp, r_human)
+    r_human_text = document.getElementById("r_human")
+    r_comp_text = document.getElementById("r_comp")
+    r_winner = document.getElementById("winner_text")
+    r_human_text.innerText = r_human
+    r_comp_text.innerText = r_comp
+    r_winner.innerText = winner  
+}
