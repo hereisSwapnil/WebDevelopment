9ebdfe4b519235b6238ea0e0b6073f9faadfb519
Swapnil Singh <swapnilskumars@gmail.com>
2023-08-28 20:23:04 +0530

    added part 2

---
+++ JS/JS-Part_2/basic.js
@@ -0,0 +1,11 @@
+// Conditional Operators
+// ==, !=, <, >, <=, >=, !==, ===
+
+// If-else-else if 
+
+// Alert
+// alert("Hello, this is an alert!");
+
+// Prompt
+// var userInput = prompt("Please enter your name:");
+// console.log("Hello, " + userInput + "!");
