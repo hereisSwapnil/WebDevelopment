df401927f143ef7d95180045fe8abdaee830d886
Swapnil Singh <swapnilskumars@gmail.com>
2023-08-28 21:24:31 +0530

    added js part 4

---
+++ JS/JS-Part_4/basic.js
@@ -0,0 +1,18 @@
+// for loop
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// Nested Loops
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 2; j++) {
+        console.log("This is loop " + (parseInt(i) + 1) + " Interation " + (parseInt(j) + 1));
+    }
+}
+
+// While Loop
+var i = 0;
+while(i<5){
+    console.log(i);
+    i++
+}
