98e6e70c86b5931decc0ec6f704ab77e9e56599a
Swapnil Singh <swapnilskumars@gmail.com>
2023-08-28 20:22:55 +0530

    added part 1

---
+++ JS/JS-Part_1/basic.js
@@ -0,0 +1,26 @@
+// Data Types
+// numbers 👉 1,3,5,7,8
+// strings 👉 "hello world", "hey"
+// arrays 👉 []
+// objects 👉 {}
+// boolean 👉 true, false
+
+// Math Operations
+// Multiply 👉 *
+// Divide 👉 /
+// Exponents 👉 **
+// Modulo/Remainder 👉 %
+// Add 👉 +
+// Subtract 👉 -
+
+// Math Method object
+// Floor 👉 Rounds Down
+// Ceil 👉 Rounds Up
+// Random 👉 Random number from 0 to 1
+
+// var 👉 not recommended to use
+// let 👉 used for local scope generally used 
+// const 👉 used if the value is not to be changed
+
+// strings
+// let name = "swamp"
